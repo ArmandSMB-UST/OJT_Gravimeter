8d042a3827d85ca398adbb752e13b59965c4dea9
ArmandSMB-UST <armandosalvador.bambao.sci@ust.edu.ph>
2021-06-01 21:48:14 +0800

    Added lines, edit variables.

--- Gravimeter/sketch.js
+++ Gravimeter/sketch.js
@@ -5,10 +5,18 @@ let bob;
 let anchor;
 let velocity;
 // spring constant
-let k = 0.01;
+let k = 0.2; //kgs^{-2}
+// Gravity related constants
 let gravity;
+let mass_max = 1.498387735*10^8 //in kg
+let G = 6.67384 * 10 **(-11) //gravitational constant in m^3/kgs^{-2}
+let density_basalt = 3000 //in kgm^{-3}
+let density_coal = 1300 //in kgm^{-3}
+//Define a volume_max = density / mass_max
+  //Take this for slider, let mass = density*volume
+  //This could be simplified as gravity_total = gravity + G*mass
 // rest length is the distance between the anchor and the bob
-let restLength = 200;
+let restLength = 200; 
 // ----------------------------------------------------------------
 
 function setup() {
@@ -16,10 +24,10 @@ function setup() {
   bob = createVector(windowWidth/2, 0);
   anchor = createVector(windowWidth/2, 0);
   velocity = createVector(0, 0);
-  gravity = createVector(0, 0.5);
+  gravity = createVector(0, 9.80665); //in ms^{-2}
 
   // slider - refer to this: createSlider(min, max, [value], [step])
-  slider = createSlider(.5, 3.5, 2, .05);
+  slider = createSlider(0, 100, 0, 0.5); //assume this is in mGal
   slider.position(10, 50);
   slider.style('width', '150px');
 }
@@ -42,14 +50,14 @@ function draw() {
   // }
 
   // vector that points from A to B
-  let force = p5.Vector.sub(bob, anchor);
+  let force = p5.Vector.sub(bob, anchor); // s0 - 0
   console.log(force);
   // displacement
-  let x = force.mag() - restLength;
+  let y = force.mag() - restLength; //change in length
   // normalize the force vector
   force.normalize();
   // spring force
-  force.mult(-1 * k * x);
+  force.mult(-1 * k * y); //F = -k(s - s0)
 
   // F = M*A (if M == 1)
   velocity.add(force);
@@ -57,7 +65,7 @@ function draw() {
   bob.add(velocity);
 
   // damping
-  velocity.mult(0.99);
+  velocity.mult(0.38);  //according to one paper
 
   // adjust gravity
   gravity.y = slider.value();
